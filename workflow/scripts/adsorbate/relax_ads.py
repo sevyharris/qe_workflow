7f09c717b44f2179f68704b933c8a78a81ebf694
Sevy Harris <harris.se@northeastern.edu>
2022-03-14 17:05:47 -0400

    add scripts for adsorbate relaxation, delete old ads scripts

---
+++ workflow/scripts/adsorbate/relax_ads.py
@@ -0,0 +1,12 @@
+# Script to relax adsorbate. Expects the adsorbate_dir and the directory with all the xyz files
+import os
+import sys
+
+import adlib.adsorbate.calc
+
+
+adsorbate_dir = sys.argv[1]
+xyz_dir = sys.argv[2]
+
+adlib.adsorbate.calc.setup_relax_adsorbate(adsorbate_dir, xyz_dir=xyz_dir)
+adlib.adsorbate.calc.run_relax_adsorbate(adsorbate_dir)
