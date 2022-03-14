90eca29cb3b9d9abf7414e574cdd6f9d37787958
Sevy Harris <harris.se@northeastern.edu>
2022-03-14 17:59:04 -0400

    added script for relaxing slab, deleted old version

---
+++ workflow/scripts/slab/relax_slab.py
@@ -0,0 +1,28 @@
+import os
+import sys
+import numpy as np
+
+from ase.io.espresso import read_espresso_out
+
+import adlib.slab.calc
+
+
+slab_dir = sys.argv[1]
+# expecting a path that looks like this:
+# path/to/dft/metal/slab
+# path/to/dft/metal/bulk
+
+
+if len(sys.argv) < 3:
+    # load the lattice constant from eos_fine
+    bulk_dir = os.path.join(os.path.dirname(slab_dir), 'bulk')
+    eos_coarse_dir = os.path.join(bulk_dir, 'eos_fine')
+    lattice_constant = adlib.bulk.eos.analyze_eos(eos_fine_dir)
+else:
+    lattice_constant = float(sys.argv[2])
+
+metal = os.path.basename(os.path.dirname(slab_dir))
+
+adlib.slab.calc.make_relax_script(slab_dir, lattice_constant, metal=metal)
+adlib.slab.calc.make_run_relax_script(slab_dir)
+adlib.slab.calc.run_relax_slab(slab_dir)
