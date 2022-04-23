c0e32eb78ab15b59a9a6cee1d559adec1f18e1c0
Sevy Harris <harris.se@northeastern.edu>
2022-04-23 08:54:36 -0400

    add low mixing_beta option to relax_system rule in Snakemake

--- workflow/scripts/system/relax_system.py
+++ workflow/scripts/system/relax_system.py
@@ -6,6 +6,15 @@ import numpy as np
 import adlib.system.calc
 
 
+# Expected arguments:
+# 1. system_dir - path to the system directory where the calculation will take place
+# 2. slab_pwo
+# 3. adsorbate_pwo
+
+# Optional arguments:
+# 4. job_name
+# 5. low_mixing_beta
+
 system_dir = sys.argv[1]
 
 os.makedirs(system_dir, exist_ok=True)
@@ -23,7 +32,13 @@ if len(sys.argv) > 4:
 else:
     job_name = 'relax_system'
 
+# Use low mixing_beta settings for certain metals (Cu, Ni)
+low_mixing_beta=False
+if len(sys.argv) > 5:
+    if sys.argv[5] == 'low' or sys.argv[5] == 'low_mixing_beta':
+        low_mixing_beta=True
 
-adlib.system.calc.make_relax_script(system_dir, nproc=48, ecutwfc=50)
+
+adlib.system.calc.make_relax_script(system_dir, nproc=48, ecutwfc=50, low_mixing_beta=low_mixing_beta)
 adlib.system.calc.make_run_relax_script(system_dir, nproc=48, job_name=job_name)
 adlib.system.calc.run_relax_system(system_dir)
